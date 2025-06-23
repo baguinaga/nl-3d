87f9fbeeeada1599a409c6d796da547ef03f04c8
Billy Aguinaga <aguinaga.b@gmail.com>
2025-06-22 23:02:05 -0400

    feature: jest testing setup

---
+++ src/app/page.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import Home from "./page";
+
+jest.mock("@xenova/transformers", () => ({
+  pipeline: jest.fn().mockResolvedValue({
+    __esModule: true,
+    default: jest.fn(() => ({})),
+  }),
+}));
+
+jest.mock("react-use-measure", () => ({
+  __esModule: true,
+  default: jest.fn(() => [jest.fn(), { width: 0, height: 0 }]),
+}));
+
+jest.mock("@react-three/fiber", () => ({
+  Canvas: jest.fn(() => null),
+}));
+
+jest.mock("@react-three/drei", () => ({
+  OrbitControls: jest.fn(() => null),
+}));
+
+jest.mock("color-name", () => ({
+  default: jest.fn(() => ({})),
+}));
+
+describe("Home Page", () => {
+  it("renders the input control", async () => {
+    render(<Home />);
+
+    const inputElement = await screen.findByPlaceholderText(/Enter command/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+});
